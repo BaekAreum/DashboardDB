4d45e1c5eba66c51c47f56d285cae6c5c24a4b19
BaekAreum <20191775@edu.hanbat.ac.kr>
2022-11-27 00:38:13 +0900

    risk

--- Dashboard/src/main/java/dashboard/fwd/web/FwdController.java
+++ Dashboard/src/main/java/dashboard/fwd/web/FwdController.java
@@ -1,13 +1,18 @@
 package dashboard.fwd.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,10 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.*;
-import java.net.*;
 
-import dashboard.fwd.domain.ClimateVO;
 import dashboard.fwd.domain.ParameterList;
 import dashboard.fwd.service.FwdService;
 
@@ -54,6 +56,9 @@ public class FwdController {
 		}else if(parameter.equals("gochang")) {
 			param.setType("tb_gochang_data_tp");
 			param.setDataName("FILE_DT");
+		}else if(parameter.equals("risk")) {
+			param.setType("ae");
+			param.setDataName("FILE_DT");
 		}
 		
 		ObjectMapper mapper = new ObjectMapper();
@@ -65,27 +70,26 @@ public class FwdController {
 	}
 	
 
+	/*
 	//../Dashboard/risk  (포트 번호 5000으로 해야 하는지 테스트 해야 됨.. 백엔드에서 실행할 때는 주소를 5000으로 해야 됐음)
 	@RequestMapping(value = "/risk", method = RequestMethod.GET)
-	public ModelAndView Test() {
+	public ModelAndView riskReturn(Model model, @RequestParam(name="sdate") String sdate, @RequestParam(name="edate") String edate) throws Exception {
 		ModelAndView mav = new ModelAndView();
+//		Model model = null;
 		String url = "http://127.0.0.1:5000/Dashboard/risk";
 		String sb = "";
-		try { 
+		try {     
 			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
 			
-
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
 
 			String line = null;
-
 			while ((line = br.readLine()) != null) {
 				sb = sb + line + "\n";
 			}
 			System.out.println("===============" + sb.toString());
 			if (sb.toString().contains("ok")) {
-				System.out.println("risk page ok");
-				
+				System.out.println("risk page ok");	
 			}
 			br.close();
 
@@ -95,13 +99,17 @@ public class FwdController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		mav.addObject("risk", sb.toString()); // "test1"는 jsp파일에서 받을때 이름, 
-        						//sb.toString은 value값(여기에선 test)
+		model.addAttribute("risk", sb.toString());
+		/*
+		mav.addObject("risk", sb.toString()); // "risk"는 jsp파일에서 받을때 이름, 
+        						//sb.toString은 value값(여기에선 risk)
 		mav.addObject("fail", false);
 		mav.setViewName("risk");   // jsp파일 이름
+		
+		System.out.println();
 		return mav;
 	}
-	
+	*/
 	
 	@GetMapping("/main.do")
 	public String mainPage() {
